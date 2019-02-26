400d401620f4a15a27b8de7cedcafe21c73276f4
Nguyen Quang Huy <huynq0911@gmail.com>
2019-02-26 16:13:54 +0700

    done database tut

--- example/database/database.go
+++ example/database/database.go
@@ -1,27 +1,74 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
 
 type Employee struct {
-	id	int
-	name	string
-	address	string
-}
-
-func insertData(db *sql.DB, employee Employee)  {
-	insert, _ := db.Query("INSERT INTO employee VALUES ("+employee.name+","+employee.address+")")
-	defer insert.Close()
+	name string `json:"name"`
+	city string `json:"city"`
 }
 
 func main() {
-	db, err := sql.Open("mysql", "huynq:1@tcp(127.0.0.1:3306)/test")
+	db, err := sql.Open("mysql", "root:1@tcp(127.0.0.1:3306)/goblog")
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
 
-
 	// insert data
+	//empl := Employee{"ABC", "HCM"}
+	//insertEmpl(db, empl)
+
+	// delete data
+	//deleteEmpl(db, "ABC")
+
+	// update data
+	updateEmpl(db, "huy", "bac giang")
+
+	// select
+	selectAll(db)
+
 }
 
+func insertEmpl(db *sql.DB, employee Employee) {
+	insert, err := db.Query("INSERT INTO employee(name, city) VALUES ('" + employee.name + "','" + employee.city + "')")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer insert.Close()
+}
 
+func deleteEmpl(db *sql.DB, emplName string) {
+	del, err := db.Query("DELETE from employee WHERE name LIKE '%" + emplName + "'")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer del.Close()
+}
+
+func updateEmpl(db *sql.DB, emplName string, newCity string) {
+	upd, err := db.Query("UPDATE employee SET city = '" + newCity + "' WHERE name LIKE '%" + emplName + "'")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer upd.Close()
+}
+
+func selectAll(db *sql.DB) {
+	results, err := db.Query("SELECT name, city FROM employee")
+	if err != nil {
+		panic(err.Error())
+	}
+	for results.Next() {
+		var employee Employee
+		err = results.Scan(&employee.name, &employee.city)
+		if err != nil {
+			panic(err.Error())
+		}
+		log.Printf(employee.name + "\t\t\t" + employee.city)
+	}
+	//defer results.Close()
+}
